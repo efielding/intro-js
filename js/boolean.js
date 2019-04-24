2368bf8e3ea309cc2d0d24b2493b596997ce51a3
Emily Waterston <emilywfielding.gmail.com>
2019-04-23 20:08:26 -0700

    learing boolean and NaN in boolean.js

---
+++ js/boolean.js
@@ -0,0 +1,11 @@
+//  boolean data type
+var a = true; // is equal to 1
+var b = false; // is equal to 0
+//  all comparisons results in a boolean answer
+console.log(12 === '12');
+console.log(12 == '12');
+
+console.log(isNaN(2000));
+console.log(isNaN('2000'));
+console.log(isNaN('abc'));
+console.log(isNaN('4/23/2019')); // because of / it is a date not numerical # so it retuns true
