cc991c9585e1293e95a47ef6f3d2b6df7562b51d
Emily Waterston <emilywfielding.gmail.com>
2019-04-24 19:37:35 -0700

    review var

---
+++ js/review-data-1.js
@@ -0,0 +1,71 @@
+//  5 interger var
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// 5 string var
+var str1 = 'one';
+var str2 = 'two';
+var str3 = 'three';
+var str4 = 'four';
+var str5 = 'five';
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(str5);
+
+//  boolean var
+var f = false;
+var g = true;
+
+console.log(5 == '5');
+console.log(5 === '5');
+
+
+// 5 array var
+arr1 = ['one', 'two'];
+arr2 = [ 'three', 'four'];
+arr3 = [ 'five', 'six'];
+arr4 = [ 'seven', 'eight'];
+arr5 = [ 'nine', 'ten'];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+console.log(arr5);
+
+// object var
+var birthday = {
+    month: 'Dec',
+    day: 6 ,
+    year: 1985,
+};
+
+var address = {
+    number: 2132,
+    street: 'Golden Ave',
+    city: 'clovis',
+    zip: 93611,
+};
+
+console.log(birthday);
+console.log(birthday.month);
+console.log(birthday.day);
+console.log(birthday.year);
+
+console.log(address);
+console.log(address.number);
+console.log(address.street);
+console.log(address.city);
+console.log(address.zip);
