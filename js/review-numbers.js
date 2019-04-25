9c075df875f2ae17bc62f5717d38a56fe556a2ec
Emily Waterston <emilywfielding.gmail.com>
2019-04-24 19:45:20 -0700

    review-numbers

---
+++ js/review-numbers.js
@@ -0,0 +1,17 @@
+var a = 10;
+var b = 3;
+
+var sum = a+b;
+console.log(sum);
+
+var diff = a-b;
+console.log(diff);
+
+var multi = a*b;
+console.log(multi);
+
+var div = b/a;
+console.log(div);
+
+var mod = a % b;
+console.log(mod);
